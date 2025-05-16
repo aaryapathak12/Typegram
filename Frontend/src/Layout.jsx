f917989ebd2db570b271d8687c59360ce028e0a0
Arya pathak <90878505+aaryapathak12@users.noreply.github.com>
2025-05-16 23:50:09 +0530

    V3-login addon

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import DisplayParagraph from './components/DisplayParagraph'
-import { RiTimer2Fill } from "react-icons/ri";
-import './App.css'
-
+import React, { useEffect } from 'react';
+import NavBar from './components/NavBar';
+import DisplayParagraph from './components/DisplayParagraph';
+import { useAuth0 } from '@auth0/auth0-react';
+import './App.css';
 
 function Layout() {
+  const {
+    user,
+    loginWithRedirect,
+    isAuthenticated,
+    isLoading,
+  } = useAuth0();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isLoading, isAuthenticated, loginWithRedirect]);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <>
-    <NavBar/>
-    <div>
-        <div className='h-full flex items-center justify-center mt-10'>
-        <div className='h-2/3 w-2/3'>
-             <DisplayParagraph paragraph={"Make sure your compiled CSS is included in the <head> (your framework might handle this for you), then start using Tailwind’s utility classes to style your content."} />
-        </div>
-    </div>
-    </div>
+      {isAuthenticated && (
+        <>
+          <NavBar />
+          <div className='h-full flex items-center justify-center mt-10'>
+            <div className='h-2/3 w-2/3'>
+              <DisplayParagraph paragraph={"Make sure your compiled CSS is included in the <head> (your framework might handle this for you), then start using Tailwind’s utility classes to style your content."} />
+            </div>
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,10 +5,21 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './App.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 
+import App from './App.jsx'
+const domain=process.env.REACT_APP_AUTH0_DOMAIN;
+const client=process.env.REACT_APP_AUTH0_CLIENT_ID;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+    domain={domain}
+    clientId={client}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+  <App />
+  </Auth0Provider>
   </StrictMode>,
 )
